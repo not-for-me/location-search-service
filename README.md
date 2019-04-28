7a937c03158d941eea805f4ec03705472718834f
woojin.joe <woojin.joe@linecorp.com>
2019-04-28 11:10:45 +0900

    Set up project envoriment

---
+++ README.md
@@ -0,0 +1,16 @@
+# Location Search Service (LSS)
+
+![build](https://img.shields.io/circleci/project/github/not-for-me/location-search.svg)
+
+## Tech Stack
+* Spring boot (Webflux)
+* JPA
+* Redis
+* H2 DB 
+
+## How to run
+T.B.D.
+
+## Design 
+T.B.D.
+
---
+++ src/main/java/kr/notforme/lss/LssApplication.java
@@ -0,0 +1,11 @@
+package kr.notforme.lss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LssApplication.class, args);
+    }
+}
